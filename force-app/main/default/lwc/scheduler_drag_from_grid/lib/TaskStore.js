3dcad992b6e918898729726d0e1dee2c167207bf
Alexey Lazarev <alex@bryntum.com>
2023-12-11 22:10:46 +0100

    fix bug with DateHelper

--- force-app/main/default/lwc/scheduler_drag_from_grid/lib/TaskStore.js
+++ force-app/main/default/lwc/scheduler_drag_from_grid/lib/TaskStore.js
@@ -1,82 +1,87 @@
-export default (EventStoreClass, Task) => class TaskStore extends EventStoreClass {
+export default (EventStoreClass, Task) => {
+    const { DateHelper } = window.bryntum.scheduler;
 
-    static $name = 'TaskStore';
 
-    static get defaultConfig() {
-        return {
-            modelClass : Task
-        };
-    }
+    return class TaskStore extends EventStoreClass {
 
-    // Override add to reschedule any overlapping events caused by the add
-    add(records, silent = false) {
-        const me = this;
+        static $name = 'TaskStore';
 
-        if (me.autoRescheduleTasks) {
-            // Flag to avoid rescheduling during rescheduling
-            me.isRescheduling = true;
-            me.beginBatch();
+        static get defaultConfig() {
+            return {
+                modelClass : Task
+            };
         }
 
-        if (!Array.isArray(records)) {
-            records = [records];
-        }
+        // Override add to reschedule any overlapping events caused by the add
+        add(records, silent = false) {
+            const me = this;
 
-        super.add(records, silent);
+            if (me.autoRescheduleTasks) {
+                // Flag to avoid rescheduling during rescheduling
+                me.isRescheduling = true;
+                me.beginBatch();
+            }
 
-        if (me.autoRescheduleTasks) {
-            me.endBatch();
-            me.isRescheduling = false;
-        }
-    }
+            if (!Array.isArray(records)) {
+                records = [records];
+            }
 
-    // Auto called when triggering the update event.
-    // Reschedule if the update caused the event to overlap any others.
-    onUpdate({ record }) {
-        if (this.autoRescheduleTasks && !this.isRescheduling)  {
-            this.rescheduleOverlappingTasks(record);
-        }
-    }
+            super.add(records, silent);
 
-    rescheduleOverlappingTasks(eventRecord) {
-        if (eventRecord.resource) {
-            const
-                futureEvents  = [],
-                earlierEvents = [];
-
-            // Split tasks into future and earlier tasks
-            eventRecord.resource.events.forEach(event => {
-                if (event !== eventRecord) {
-                    if (event.startDate >= eventRecord.startDate) {
-                        futureEvents.push(event);
-                    }
-                    else {
-                        earlierEvents.push(event);
-                    }
-                }
-            });
-
-            if (futureEvents.length || earlierEvents.length) {
-                futureEvents.sort((a, b) => a.startDate > b.startDate ? 1 : -1);
-                earlierEvents.sort((a, b) => a.startDate > b.startDate ? -1 : 1);
-
-                futureEvents.forEach((ev, i) => {
-                    const prev = futureEvents[i - 1] || eventRecord;
-
-                    ev.startDate = DateHelper.max(prev.endDate, ev.startDate);
-                });
-
-                // Walk backwards and remove any overlap
-                [eventRecord, ...earlierEvents].forEach((ev, i, all) => {
-                    const prev = all[i - 1];
-
-                    if (ev.endDate > Date.now() && ev !== eventRecord && prev) {
-                        ev.setEndDate(DateHelper.min(prev.startDate, ev.endDate), true);
-                    }
-                });
-
-                this.isRescheduling = false;
+            if (me.autoRescheduleTasks) {
+                me.endBatch();
+                me.isRescheduling = false;
             }
         }
-    }
-};
+
+        // Auto called when triggering the update event.
+        // Reschedule if the update caused the event to overlap any others.
+        onUpdate({ record }) {
+            if (this.autoRescheduleTasks && !this.isRescheduling)  {
+                this.rescheduleOverlappingTasks(record);
+            }
+        }
+
+        rescheduleOverlappingTasks(eventRecord) {
+            if (eventRecord.resource) {
+                const
+                    futureEvents  = [],
+                    earlierEvents = [];
+
+                // Split tasks into future and earlier tasks
+                eventRecord.resource.events.forEach(event => {
+                    if (event !== eventRecord) {
+                        if (event.startDate >= eventRecord.startDate) {
+                            futureEvents.push(event);
+                        }
+                        else {
+                            earlierEvents.push(event);
+                        }
+                    }
+                });
+
+                if (futureEvents.length || earlierEvents.length) {
+                    futureEvents.sort((a, b) => a.startDate > b.startDate ? 1 : -1);
+                    earlierEvents.sort((a, b) => a.startDate > b.startDate ? -1 : 1);
+
+                    futureEvents.forEach((ev, i) => {
+                        const prev = futureEvents[i - 1] || eventRecord;
+
+                        ev.startDate = DateHelper.max(prev.endDate, ev.startDate);
+                    });
+
+                    // Walk backwards and remove any overlap
+                    [eventRecord, ...earlierEvents].forEach((ev, i, all) => {
+                        const prev = all[i - 1];
+
+                        if (ev.endDate > Date.now() && ev !== eventRecord && prev) {
+                            ev.setEndDate(DateHelper.min(prev.startDate, ev.endDate), true);
+                        }
+                    });
+
+                    this.isRescheduling = false;
+                }
+            }
+        }
+    };
+}
