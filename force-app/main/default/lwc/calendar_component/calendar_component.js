ae4caf7cc8cf9e6c56e53698873e4f1e3230caba
Maxim Gorkovskii <ragzovskii@live.ru>
2025-12-12 15:37:45 +0400

    update calendar lwc

--- force-app/main/default/lwc/calendar_component/calendar_component.js
+++ force-app/main/default/lwc/calendar_component/calendar_component.js
@@ -3,7 +3,7 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import CALENDAR from '@salesforce/resourceUrl/bryntum_calendar';
-import { RESOURCES, EVENTS } from './data/data';
+import { data } from './data/data';
 
 export default class Calendar_component extends LightningElement {
     renderedCallback() {
@@ -35,17 +35,32 @@ export default class Calendar_component extends LightningElement {
     }
 
     createCalendar() {
-        window.calendar = new bryntum.calendar.Calendar({
+        const calendar = window.calendar = new bryntum.calendar.Calendar({
             appendTo : this.template.querySelector('.container'),
 
             // Start life looking at this date
             date : new Date(2020, 9, 12),
 
-            eventStore : {
-                data : EVENTS
-            },
-            resourceStore : {
-                data : RESOURCES
+            // 'day', 'week', 'month', etc.
+            mode : 'week',
+
+            crudManager : {},
+
+            sidebar : {
+                items : {
+                    datePicker : {
+                        // highlight the selected cell's week row
+                        highlightSelectedWeek : true
+                    },
+                    compactHeader : {
+                        weight : 100,
+                        type   : 'slidetoggle',
+                        text   : 'Show compact header',
+
+                        // "up." means resolve in owner will call on the Calendar
+                        onChange : 'up.onToggleCompactHeader'
+                    }
+                }
             },
 
             // Features named by the properties are included.
@@ -56,7 +71,17 @@ export default class Calendar_component extends LightningElement {
                     // We want the tooltip's left edge aligned to the right edge of the event if possible.
                     align : 'l-r'
                 }
+            },
+
+            onToggleCompactHeader({ checked }) {
+                this.eachView(v => {
+                    if (v.isDayView) {
+                        v.compactHeader = checked;
+                    }
+                });
             }
         });
+
+        calendar.crudManager.loadCrudManagerData(data).then(() => {});
     }
 }
